c948c9eb780962f192943a0285d09f771519afe3
kozheshkurt <122646879+kozheshkurt@users.noreply.github.com>
2024-02-11 17:37:02 +0200

    Caller adding and 5 contacts giving
    Function of adding the user to Callers sheet. Function of giving them 5 free contacts snd filling these contacts with Caller's name.

--- bot.py
+++ bot.py
@@ -11,10 +11,29 @@ bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     if message.from_user.last_name == None:
-        mess = f'Hello, <b>{message.from_user.first_name}</b>'
+        mess = f'Hello, <b>{message.from_user.first_name}</b>. '
     else:
-        mess = f'Hello, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
+        mess = f'Hello, <b>{message.from_user.first_name} {message.from_user.last_name}</b>. '
+    caller_info = [message.from_user.id, message.from_user.first_name, message.from_user.last_name]
+    mess = mess + proc.add_caller(caller_info) # adds caller info as a row to the google spreadsheet, if the user doesn't exist
     bot.send_message(message.chat.id, mess, parse_mode='html')
+
+@bot.message_handler(commands=['give_5_contacts'])
+def give_5_contacts(message):
+    free_contacts = proc.empty_contacts() #list of contacts, that don't have field 'Caller'
+
+    if len(free_contacts) < 1:
+        bot.send_message(message.chat.id, 'There are no free contacts to call')
+    else:
+        if len(free_contacts) < 5:
+            N = len(free_contacts)
+        else:
+            N = 5
+    for i in range(N):
+        mess =', '.join(free_contacts[i][:6]) 
+        print(mess)
+        proc.add_caller_to_contact(free_contacts[i], message.from_user.first_name)
+        bot.send_message(message.chat.id, mess)
 
 '''
 @bot.message_handler(commands=['start'])
@@ -29,4 +48,4 @@ def get_user_text(message):
     bot.send_message(message.chat.id, message.text + mess)
 '''
 
-bot.polling(none_stop=True)
+bot.infinity_polling()
--- proc.py
+++ proc.py
@@ -4,27 +4,70 @@ import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials	
 
-
-# Функция добавляет данные в низ таблицы (в первые незаполненные строки)
-def add_into_bottom (sheetTitle, values): #values - список списков (строк) [['A1','B1'],['A2','B2'],['A3','B3']]
-    # Проверяем, сколько заполненных строк в столбце A (ID)
+# класс обзвонщика
+class Caller:
+    def __init__(self, Id): # при создании присваиваем Id
+        self.Id = Id
+        
+        self.contacts_ids = []
     
+    def add_caller (self):
+        pass
+
+
+def authorize():
+    CREDENTIALS_FILE = 'mypython-rbecs-calls-081fcae8bfff.json'  # Имя файла с закрытым ключом, вы должны подставить свое
+
+    # Читаем ключи из файла
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
+
+    httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в системе
+    service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API 
+    print ('Authorization success')
+    return service
+
+
+def get_sheetlist():
+    # Получаем список листов, их Id и название
+    spreadsheet = service.spreadsheets().get(spreadsheetId = spreadsheetId).execute()
+    sheetList = spreadsheet.get('sheets')
+    print ('Sheets:')
+    for sheet in sheetList:
+        print('ID = ', sheet['properties']['sheetId'], ', title = ', sheet['properties']['title'])
+
+    return sheetList
+
+# Получаем список строк, каждая в формате [id, name, number, school, region, town, caller_name, result]
+def get_all_rows(sheetTitle):
+    # читаем данные из таблицы
+    request = service.spreadsheets().values().batchGet(spreadsheetId = spreadsheetId, 
+                                     ranges = [sheetTitle+"!A:H"], 
+                                     valueRenderOption = 'FORMATTED_VALUE',  
+                                     dateTimeRenderOption = 'FORMATTED_STRING')
+    response = request.execute() 
+    # читаем данные из листа
+    sheet_values = response['valueRanges'][0]['values']
+    return sheet_values
+
+
+# Функция добавляет данные в низ таблицы (в первую незаполненную строку)
+def add_one_into_bottom (sheetTitle, values): #values - список данных в строке ['A1','B1','C1']
+    # Проверяем, сколько заполненных строк в столбце A (ID)
     request = service.spreadsheets().values().batchGet(spreadsheetId = spreadsheetId, 
                                         ranges = [sheetTitle+"!A:A"], 
                                         valueRenderOption = 'FORMATTED_VALUE',  
                                         dateTimeRenderOption = 'FORMATTED_STRING')
-    response = request.execute() 
+    response = request.execute()
     sheet_values = response['valueRanges'][0]['values']
-
-    range = sheetTitle + "!A" + str(len(sheet_values) + 1) + ":H" + str(len(sheet_values) + 1 + len(values)) # Диапазон от A до H, номера строк с последней заполненной+1 
-    
-    # Заполняем данными строки в нужном диапазоне
+    # Проверяем, есть ли уже ID в списке
+    range = sheetTitle + "!A" + str(len(sheet_values) + 1) + ":H" + str(len(sheet_values) + 1) # Диапазон от A до H, номера строк с последней заполненной + 1
+    # Заполняем данными строку в нужном диапазоне
     request = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheetId, body = {
         "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
         "data": [
             {"range": range, 
             "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
-            "values": values}
+            "values": [values]}
         ]
     })
     response = request.execute()
@@ -32,65 +75,79 @@ def add_into_bottom (sheetTitle, values): #values - список списков 
     return response
 
 
-CREDENTIALS_FILE = 'mypython-rbecs-calls-081fcae8bfff.json'  # Имя файла с закрытым ключом, вы должны подставить свое
+def add_caller(caller_info):
+    callers_sheetTitle = sheetList[1]['properties']['title']
+    existing_callers = get_all_rows(callers_sheetTitle)
+    if str(caller_info[0]) in [caller[0] for caller in existing_callers]:
+        print('The user is already in the table. ', caller_info)
+        return 'The user is already in the table'
+    else:
+        add_one_into_bottom (callers_sheetTitle, caller_info)
+        #caller = Caller(caller_info[0])
+        print('User added success: ', caller_info)
+        return 'Added successfully'
 
-# Читаем ключи из файла
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
 
-httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в системе
-service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API 
+def empty_contacts():
+    contacts = get_all_rows('Contacts')
+    free_contacts = []
+    for contact in contacts:
+        if (len(contact)<7) or (contact[6] == ''):
+            free_contacts.append(contact)
+    return free_contacts
+
+
+def add_caller_to_contact(changed_contact, caller):
+    contacts = get_all_rows('Contacts')
+    for contact in contacts:
+        if str(contact[0]) == str(changed_contact[0]):
+            contact.append(caller)
+    request = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheetId, body = {
+        "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
+        "data": [
+            {"range": sheetTitle+"!A:I",
+            "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
+            "values": contacts
+            }
+        ]
+    })
+    response = request.execute()
+    
+
+
 
 spreadsheetId = '1YMX1iV-9VBS4gwn1ut6A-X3uP5COzVWa4O-MWY0aLX4'
-
-
-# Получаем список листов, их Id и название
-spreadsheet = service.spreadsheets().get(spreadsheetId = spreadsheetId).execute()
-sheetList = spreadsheet.get('sheets')
-for sheet in sheetList:
-    print('ID = ', sheet['properties']['sheetId'], ', title = ', sheet['properties']['title'])
-
+service = authorize()
+sheetList = get_sheetlist()
 # Первый лист в списке листов    
 sheetId = sheetList[0]['properties']['sheetId']
 sheetTitle = sheetList[0]['properties']['title']
 
-print('Current sheet Id = ', sheetId, '\n')
 
-# Заполняем таблицу данными 
+
+
 '''
+# Заполняем таблицу данными 
 request = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheetId, body = {
     "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
     "data": [
         {"range": sheetTitle+"!B2:D5",
-         "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
-         "values": [
+        "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
+        "values": [
                     ["Ячейка B2", "Ячейка C2", "Ячейка D2"], # Заполняем первую строку
                     ['25', "=6*6", "=SIN(3.14/2)"]  # Заполняем вторую строку
-                   ]}
+                ]}
     ]
 })
 response = request.execute()
 '''
-# Добавляем строку с тестовыми данными
-values = [['Нове прізвище',"'0501111111",'Школа','Область','Місто','Обзвонщик', 'все ок']]
-add_into_bottom (sheetTitle, values)
-
-
-# Читаем данные из таблицы
-request = service.spreadsheets().values().batchGet(spreadsheetId = spreadsheetId, 
-                                     ranges = [sheetTitle+"!A:H"], 
-                                     valueRenderOption = 'FORMATTED_VALUE',  
-                                     dateTimeRenderOption = 'FORMATTED_STRING')
-response = request.execute() 
-sheet_values = response['valueRanges'][0]['values']
-for row in sheet_values:
-    print(row)
-
 
 '''
-callers = []
-contacts = []
+# Добавляем строку с тестовыми данными
+values = ['_','Нове прізвище',"'0501111111",'Школа','Область',None,'Обзвонщик']
+add_one_into_bottom (sheetTitle, values)
 
-# добавляет обзвонщика в список обзвонщиков
-def add_caller (name):
-    callers.append(name)
+contacts = get_all_rows(sheetTitle, sheetId)
+for contact in contacts:
+    print(contact)
 '''
